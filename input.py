32c6137308db9ef052a46a347e0f8ab24db5fc2b
kaliborg04 <kaliborg04@gmail.com>
2024-04-15 17:18:15 +0200

    Ввод числа

--- input.py
+++ input.py
@@ -1,4 +1,9 @@
-s = input()
-print("Ввод:", s)
+s = input("Введите строку: ")
+print("Строка:", s)
 n = len(s)
 print('Длина:', n)
+
+print()
+s = input("Введите число: ")
+n = int(s)
+print(n, type(n))
