9862f6dd980396061ea70dbbb24c2666fe342ed9
kaliborg04 <kaliborg04@gmail.com>
2024-04-18 20:42:28 +0200

    Add ifs

--- input.py
+++ input.py
@@ -2,8 +2,15 @@ s = input("Введите строку: ")
 print("Строка:", s)
 n = len(s)
 print('Длина:', n)
+if n > 5:
+    print("Строка длинная")
+if s == "дурак":
+    print("сам дурак!")
+    exit(1)
 
 print()
 s = input("Введите число: ")
 n = int(s)
 print(n, type(n))
+if n >= 10000:
+    print("Число большое")
