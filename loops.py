0a328bc7b7ba1cd5484b1070aecbc24ed32cdc25
kaliborg04 <kaliborg04@gmail.com>
2024-04-21 11:08:14 +0200

    make new cycles

--- loops.py
+++ loops.py
@@ -1,3 +1,9 @@
 s = input("Введите строку: ")
 while s != "bye":
     print("Hello")
+f = input("Введите строку: ")
+while f == "bye":
+    print("goodbye")
+b = input("Введите строку: ")
+while b != "hello":
+    print("coat")
