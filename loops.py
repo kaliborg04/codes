ffd97d12e30faf5add909611b291077dfabe9545
kaliborg04 <kaliborg04@gmail.com>
2024-04-25 17:39:07 +0200

    Fix last loop condition

--- loops.py
+++ loops.py
@@ -1,16 +1,26 @@
+# to reach the end, input:
+# 1. i
+# 2. hi
+# 3. hello
+# 4. oh my coat
+# 5. ""
+
 s = input("Введите строку: ")
-while s != "byrrrrrr":
+while s == "hi":
     print("Hello")
+
 f = input("Введите строку: ")
 while f == "bye":
     print("goodbye")
+
 b = input("Введите строку: ")
 while b != "hello":
     print("coat")
-st = input("Введите строку:")
+
+st = input("Введите строку: ")
 while st != "oh my coat":
     print("oh my coat")
-coat = input("Введите строку:")
-while coat >= s: 
-    print(coat + s)
+
+coat = input("Введите строку: ")
+while len(coat) >= len(s):
     print("ok")
