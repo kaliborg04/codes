b63f00ab99bc8b0e443e935d015a20bbb20b5bbd
kaliborg04 <kaliborg04@gmail.com>
2024-04-26 17:13:29 +0200

    Add while-true loop

--- loops2.py
+++ loops2.py
@@ -2,6 +2,9 @@ s = input("Enter string: ")
 while len(s) != 0:
     print(s, len(s))
     s = input("Enter string: ")
-s = input("Enter string: ")
-while len(s) > 5:
-    print(s, len(s))
+
+while True:
+    s = input("Enter number: ")
+    n = int(s)
+    if n > 100:
+        break
