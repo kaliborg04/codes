b8ddf1fc1077135c33c7629ebd6b9107f6b16d1c
kaliborg04 <kaliborg04@gmail.com>
2024-05-04 20:42:44 +0200

    Modified file sum.py

--- sum.py
+++ sum.py
@@ -3,3 +3,19 @@ s2 = input("Enter number: ")
 n1 = int(s1)
 n2 = int(s2)
 print(n1 + n2)
+
+if n1 == 0:
+    print("NO!")
+    exit()
+
+if n2 == 0:
+    print("NO!")
+    exit()
+
+if n1 < 5:
+    print("NO NO NO NO NO NO NO NOOOOOOOOOOOOOOO!")
+    exit()
+
+if n2 < 5:
+    print("NO NO NO NO NO NO NO NOOOOOOOOOOOOOO)!")
+    exit()
