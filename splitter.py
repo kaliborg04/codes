cfe126d9bb01c003df043ce20cf0129dc6d16eac
kaliborg04 <kaliborg04@gmail.com>
2024-05-09 18:49:35 +0200

    Add file splitter.py

---
+++ splitter.py
@@ -0,0 +1,12 @@
+while True:
+    s = input('Enter sentence: ')
+    if s == '':
+        break
+    s = s.strip()
+    s = s.rstrip('.!?')
+    s = s.replace(",", " ").replace("-", " ")
+    s = s.lower()
+    words = s.split()
+    s = " ".join(words)
+    print(len(words), "word(s):", s)
+    
