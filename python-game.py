480cac64642bfd049d691bdb07a0e72f214fc2dc
kaliborg04 <kaliborg04@gmail.com>
2024-05-20 17:35:29 +0200

    Fix bugs in python-game.py

--- python-game.py
+++ python-game.py
@@ -1,11 +1,21 @@
-s = input("Enter city: ")
-print('\x1b[2J')
+print('\x1b[2J', end='')
+print('\x1b[1;1f', end='')
+
+cities = {}
 while True:
+    s = input("Enter city: ")
+    s = s.lower().replace("-", " ")
+
     n = input("Enter name: ")
     if len(n) == 0:
         print(n)
         break
-    n.lower()
-    n.replace(",", " ").replace("-", " ")
+    n = n.lower()
+    n = n.replace(",", " ").replace("-", " ")
     print(s, n)
 
+    if s not in cities:
+        cities[s] = [n]
+    else:
+        cities[s] += [n]
+print(cities)
