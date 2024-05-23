175cc99f97ee365e764a623c6d3ffed4fc7f48c5
kaliborg04 <kaliborg04@gmail.com>
2024-05-23 16:34:36 +0200

    Add screensaver

--- screensaver.py
+++ screensaver.py
@@ -1,4 +1,6 @@
 import subprocess as sp
+import random
+import time
 
 out = sp.check_output(["stty", "size"])
 out = out.decode().rstrip().split()
@@ -8,11 +10,15 @@ cols = int(out[1])
 print("\x1b[2J")
 print("\x1b[1;1f", end="")
 
-r = 1
-while r <= rows:
-    c = 1
-    while c <= cols:
-        print('\x1b[', r, ';', c, 'f', end='')
-        print('A', end='')
-        c += 1
-    r += 1
+while True:
+    r = 1
+    while r <= rows:
+        c = 1
+        while c <= cols:
+            print('\x1b[', r, ';', c, 'f', end='')
+            color = random.randint(41, 47)
+            print(f'\x1b[{color}m', end='')
+            print(' ', end='')
+            c += 1
+        r += 1
+    time.sleep(0.1)
