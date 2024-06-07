ea491c78ec89238b21cc3fe450c7b9c31f5b4dc4
kaliborg04 <kaliborg04@gmail.com>
2024-06-07 20:17:08 +0500

    Add time-monstr.py

---
+++ time-monstr.py
@@ -0,0 +1,11 @@
+monstor = 17
+c = 0
+while monstor >= c:
+    m = input('Enter number: ')
+    o = input('Enter srin: ')
+    if m == 0:
+        break
+    if o == 0:
+        break
+monstor -= 1
+print('GOOD', number, scrin)
