11aa767be1e110f346bd17becc76d77ddc69f34e
kaliborg04 <kaliborg04@gmail.com>
2024-06-14 09:46:52 +0500

    Add variablesi.py

---
+++ variables.py
@@ -0,0 +1,37 @@
+a = 4
+a
+a * 5
+a + a
+a = 54
+a
+a + 5
+b = 10
+a + b
+c = 1
+c = 4
+age = 20
+type(age)
+maney = 1.5
+type(maney)
+a = 4
+a = 5.6
+a = 'hello'
+b = 10
+b = b*2
+b
+b = b*2
+b
+a = 5
+c = b + 2,a
+a,b = 2,7
+a = 2
+b = 7
+print(b, a)
+a = b
+b = a
+b
+a
+t = a
+a = b
+t = b
+
