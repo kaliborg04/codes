0c7861e539f50ae016a639cdbebc5896bdef76c7
kaliborg04 <kaliborg04@gmail.com>
2024-06-18 18:48:41 +0500

    Add egoroff

---
+++ egoroff.py
@@ -0,0 +1,23 @@
+print('hello')
+print("hello world")
+print('''hello
+      world''')
+s = 'hello'
+b = 'world'
+s + ' ' + b
+c = input()
+d = ' '
+e = ''
+print('a' * 5)
+print(s * 3)
+print('abc' + str(3))
+print(len('abc'))
+print(len(''))
+z = '56789'
+print('6' in z)
+print('87' in z)
+print('aaa' == 'aaa')
+print('AAA' == 'aaa')
+print('abc' > 'r')
+print(ord('a'))
+print(ord('A'))
