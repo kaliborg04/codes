2333e7bb23e017f0c0f80454a856ec823250e47a
kaliborg04 <kaliborg04@gmail.com>
2024-06-20 20:21:17 +0500

    Add egoroff10

---
+++ egoroff/egoroff10.py
@@ -0,0 +1,16 @@
+s = 'hello boy'
+print(s)
+print(s[6])
+print('good'[3])
+d = 'buy'
+print(d[2])
+print(s[len(s) -1])
+print(s[-1])
+print(s[2:7])
+print(s[2: ])
+print(s[ : 2])
+print(s[:])
+print(s[::2])
+print(s[::-1])
+print(s[:4]+ 'i'+ s [5:])
+
