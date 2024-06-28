9b6a9df587528e46d1671bb8b9a395fbbae70a75
kaliborg04 <kaliborg04@gmail.com>
2024-06-28 17:31:52 +0500

    Add live-or-died

---
+++ live-or-died.py
@@ -0,0 +1,13 @@
+
+live = int(input('Enter 1 or enter: '))
+died = int(input('Enter -1 or enter: '))
+if died == -1:
+    print('oh no you are died')
+if died == 0:
+    print('you are not died')
+if live == 1:
+    print('you are live')
+if live == 0:
+    print('you are not live')
+
+
