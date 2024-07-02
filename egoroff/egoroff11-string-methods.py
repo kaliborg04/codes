551ddee1b2567e75a147b19b7ebbe08969d319f2
kaliborg04 <kaliborg04@gmail.com>
2024-07-02 16:09:12 +0500

    Add egoroff11-string-methods.py

---
+++ egoroff/egoroff11-string-methods.py
@@ -0,0 +1,23 @@
+print('great'.upper())
+print('great13706'.upper())
+print('FAC'.lower())
+g = 'fac y '
+print(g.upper())
+print(g.lower())
+print('daxfhjrdfffsf'.count('f'))
+print('fsfegegffbdf'.count('f', 3))
+print('fdbxjefudufj'.count('f', 1, 9))
+print(g.find('f'))
+print(g.find('w'))
+print(g.rfind('f'))
+print(g.replace('f', '!!!!!!!!'))
+print(g.isalpha())
+print(g.isdigit())
+d = '473491'
+print(int(d))
+w = '111'
+print(w.rjust(1))
+h = 'nikita nikitavich nikitav'
+print(h.split())
+print(len(h.split()))
+print(h.split('i'))
