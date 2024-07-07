3ffceeed32dfb82fadd6fad0dc8dc342b67dcd29
kaliborg04 <kaliborg04@gmail.com>
2024-07-07 18:51:31 +0500

    Add egoroff12.py

---
+++ egoroff/egoroff12.py
@@ -0,0 +1,26 @@
+april = [6,9,2,5]
+print(april)
+s = [True, 'abvgd', [49]]
+print(s)
+b = []
+print(b)
+print(type(b))
+print(len([2,5,8,0]))
+print(len(s))
+print(len(b))
+print([9, 3, 1] + [8, 1, 9])
+s = s + [190]
+print(s)
+s = ['hi'] + s
+print(s)
+print([9] * 9)
+print(49 in s)
+y = [1, 6, -9, 0, 34, 100, 78, 33, 99]
+print(y)
+print(max(y))
+print(min(y))
+print(sum(y))
+print(sorted(y))
+print(sorted(y, reverse = True))
+print([1485, 850] > [25, 69, 959])
+print([4680] == [4680])
