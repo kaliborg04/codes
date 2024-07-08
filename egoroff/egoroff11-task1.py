7130b9984b19cb6edb757a61db0ef6930bccd04b
kaliborg04 <kaliborg04@gmail.com>
2024-07-08 19:01:42 +0500

    Add egoroff11-task1.py

---
+++ egoroff/egoroff11-task1.py
@@ -0,0 +1,9 @@
+string = input("Enter string: ")
+string = string.upper()
+print(string)
+string = string.split(' ')
+print(string)
+string = string[0]
+print(string)
+string = string[1]
+print(string)
