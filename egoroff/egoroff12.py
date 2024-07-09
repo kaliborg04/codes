3567b31e78c8ed790d50e07062297bed02c1036b
kaliborg04 <kaliborg04@gmail.com>
2024-07-09 17:20:16 +0500

    Add comments to egoroff12.py

--- egoroff/egoroff12.py
+++ egoroff/egoroff12.py
@@ -1,26 +1,26 @@
 april = [6,9,2,5]
-print(april)
+print(april) # [6, 9, 2, 5]
 s = [True, 'abvgd', [49]]
-print(s)
+print(s) # [True, 'abvgd', [49]]
 b = []
-print(b)
-print(type(b))
-print(len([2,5,8,0]))
-print(len(s))
-print(len(b))
-print([9, 3, 1] + [8, 1, 9])
-s = s + [190]
+print(b) # []
+print(type(b)) # <class 'list'>
+print(len([2,5,8,0])) # 4
+print(len(s)) # 3
+print(len(b)) # 0 
+print([9, 3, 1] + [8, 1, 9]) # [9, 3, 1, 8, 1, 9]
+s = s + [190] # [True, 'abvgd', [49], 190]
 print(s)
-s = ['hi'] + s
-print(s)
-print([9] * 9)
-print(49 in s)
-y = [1, 6, -9, 0, 34, 100, 78, 33, 99]
-print(y)
-print(max(y))
-print(min(y))
-print(sum(y))
-print(sorted(y))
-print(sorted(y, reverse = True))
-print([1485, 850] > [25, 69, 959])
-print([4680] == [4680])
+s = ['hi'] + s # ['hi', True, 'abvgd' [49, 190]]
+print(s) 
+print([9] * 9) # [9, 9, 9, 9, 9, 9, 9, 9, 9]
+print(49 in s) # False
+y = [1, 100,]
+print(y) 
+print(max(y)) # 100
+print(min(y)) # -9
+print(sum(y)) # 126
+print(sorted(y)) # [1, 100]
+print(sorted(y, reverse = True) ) # [100 , 1]
+print([850] > [25, 44]) # True
+print([4680] == [4680]) # True
