d470249a5ba669064bcb3b3a4d69919cc237ac35
kaliborg04 <kaliborg04@gmail.com>
2024-07-09 17:24:49 +0500

    Add comments to egoroff11-string-methods.py

--- egoroff/egoroff11-string-methods.py
+++ egoroff/egoroff11-string-methods.py
@@ -1,23 +1,23 @@
-print('great'.upper())
-print('great13706'.upper())
-print('FAC'.lower())
+print('great'.upper()) # 'GREAT'
+print('great13706'.upper()) # 'GREAT13706'
+print('FAC'.lower()) # 'fac'
 g = 'fac y '
-print(g.upper())
-print(g.lower())
-print('daxfhjrdfffsf'.count('f'))
-print('fsfegegffbdf'.count('f', 3))
-print('fdbxjefudufj'.count('f', 1, 9))
-print(g.find('f'))
-print(g.find('w'))
-print(g.rfind('f'))
-print(g.replace('f', '!!!!!!!!'))
-print(g.isalpha())
-print(g.isdigit())
+print(g.upper()) # 'FAC Y'
+print(g.lower()) # 'fac y'
+print('daxfhjrdfffsf'.count('f')) # 5
+print('fsfegegffbdf'.count('f', 3)) # 3
+print('fdbxjefudufj'.count('f', 1, 9)) # 1
+print(g.find('f')) # 0
+print(g.find('w')) # -1
+print(g.rfind('f')) # 0
+print(g.replace('f', '!!!!!!!!')) # !!!!!!!!ac y
+print(g.isalpha()) # False
+print(g.isdigit()) # False
 d = '473491'
-print(int(d))
+print(int(d)) # 473491
 w = '111'
-print(w.rjust(1))
+print(w.rjust(1)) # '111'
 h = 'nikita nikitavich nikitav'
-print(h.split())
+print(h.split()) # ['nikita', 'nikitavich', 'nikitav']
 print(len(h.split()))
 print(h.split('i'))
