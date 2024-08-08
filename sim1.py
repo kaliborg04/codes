d8cd16f57acebc58d2b981c39ea5c96d68fa0fa7
kaliborg04 <kaliborg04@gmail.com>
2024-08-08 12:03:10 +0500

    Add part

--- sim1.py
+++ sim1.py
@@ -12,16 +12,20 @@ draw_opts = pymunk.pygame_util.DrawOptions(screen)
 
 body = pymunk.Body()
 body.position = (250, 0)
-shape = pymunk.Circle(body, 30)
+shape = pymunk.Circle(body, 10)
 shape.mass = 1
 shape.elasticity = 0.7
 shape.friction = 1
 space.add(body, shape)
 
-line1 = pymunk.Segment(space.static_body, (0, 460), (500, 98), 0)
+line1 = pymunk.Segment(space.static_body, (160, 320), (400, 266), 0)
 line1.elasticity = 0.7
 line1.friction = 1
 space.add(line1)
+line2 = pymunk.Segment(space.static_body, (40, 200), (280, 220), 2)
+line2.elasticity = 0.7
+line2.friction = 1
+space.add(line2)
 
 while True:
    space.step(1/60)
