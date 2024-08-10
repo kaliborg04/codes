50d9479d44ab5f20ae52444048a7b8d95e22d39a
kaliborg04 <kaliborg04@gmail.com>
2024-08-10 17:56:29 +0500

    Abdate sim1.py

--- sim1.py
+++ sim1.py
@@ -11,21 +11,36 @@ space.gravity = (0, 1000)
 draw_opts = pymunk.pygame_util.DrawOptions(screen)
 
 body = pymunk.Body()
-body.position = (250, 0)
+body.position = (350, 0)
 shape = pymunk.Circle(body, 10)
 shape.mass = 1
 shape.elasticity = 0.7
 shape.friction = 1
 space.add(body, shape)
 
-line1 = pymunk.Segment(space.static_body, (450, 50), (150, 150),2)
+line1 = pymunk.Segment(space.static_body, (250, 150), (480, 70), 2)
 line1.elasticity = 0.7
 line1.friction = 1
 space.add(line1)
-line2 = pymunk.Segment(space.static_body, (50, 150), (300, 250), 2)
+
+line2 = pymunk.Segment(space.static_body, (50, 150), (280, 240), 2)
 line2.elasticity = 0.7
 line2.friction = 1
 space.add(line2)
+
+line3 = pymunk.Segment(space.static_body, (250, 350), (480, 270), 2)
+line3.elasticity = 0.7
+line3.friction = 1
+space.add(line3)
+
+line4 = pymunk.Segment(space.static_body, (50, 400), (500, 500), 2)
+line4.elasticity = 0.7
+line4.friction = 1
+space.add(line4)
+stop_line = pymunk.Segment(space.static_body, (500,  500), (500, 460), 3)
+stop_line.elasticity = 0.7
+stop_line.friction = 1                       
+space.add(stop_line)
 
 while True:
    space.step(1/60)
