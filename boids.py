e9e3b27136c2c783990c8569e56d6a02e1cb927a
kaliborg04 <kaliborg9000@gmail.com>
2025-01-27 10:41:14 +0200

    Add colors to boids

--- boids.py
+++ boids.py
@@ -7,13 +7,20 @@ pygame.init()
 screen = pygame.display.set_mode([500, 500])
 clock = pygame.time.Clock()
 
+colors = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+]
 boids = []
 for i in range(N):
     x = random.randint(0, 500)
     y = random.randint(0, 500)
     vx = random.randint(-2, 2)
     vy = random.randint(-2, 2)
-    boid = {'x': x, 'y': y, 'vx': vx, 'vy': vy}
+    color_idx = random.randint(0, len(colors) - 1)
+    color = colors[color_idx]
+    boid = {'x': x, 'y': y, 'vx': vx, 'vy': vy, 'color': color}
     boids.append(boid)
 
 while True:
@@ -21,6 +28,6 @@ while True:
     for boid in boids:
         boid['x'] += boid['vx']
         boid['y'] += boid['vy']
-        pygame.draw.circle(screen, (255, 0, 0), (boid['x'], boid['y']), 5)
+        pygame.draw.circle(screen, boid['color'], (boid['x'], boid['y']), 5)
     pygame.display.flip()
     clock.tick(60)
