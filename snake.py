bf65a86bdc9fc87d828c44a88e55cc04c8f44445
kaliborg04 <kaliborg9000@gmail.com>
2025-03-05 11:23:33 +0200

    snake: control with keyboard

--- snake.py
+++ snake.py
@@ -10,14 +10,32 @@ snake = [
     {'xc': 26, 'yc': 25},
 ]
 
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
+
 speed = 2
+direction = RIGHT
 dist = 0
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                direction = LEFT
+            elif event.key == pygame.K_RIGHT:
+                direction = RIGHT
+            elif event.key == pygame.K_UP:
+                direction = UP
+            elif event.key == pygame.K_DOWN:
+                direction = DOWN
+
     t = clock.tick(60) / 1000
     dist += speed * t
     print(t, dist)
     if dist >= 1: 
         dist -= 1
+
         for i in range(len(snake) - 1):
             cell = snake[i]
             nxt = snake[i + 1]
@@ -25,7 +43,14 @@ while True:
             cell['yc'] = nxt['yc']
 
         head = snake[-1]
-        head['xc'] += 1
+        if direction == RIGHT:
+            head['xc'] += 1
+        elif direction == LEFT:
+            head['xc'] -= 1
+        elif direction == UP:
+            head['yc'] -= 1
+        elif direction == DOWN:
+            head['yc'] += 1
 
     screen.fill((0, 0, 0))
     for i, cell in enumerate(snake):
