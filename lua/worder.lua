3b40d6a7f8a27b9240af6dd60a2ebed04a108199
kaliborg04 <kaliborg9000@gmail.com>
2025-08-09 21:29:51 +0500

    fixed errors

--- lua/worder.lua
+++ lua/worder.lua
@@ -1,7 +1,9 @@
 local words = {}
 while true do
+    io.write("Enter number: ")
     local w = io.read()
     if w == nil or w == '' then
+        table.sort(words)
         for _, word in ipairs(words) do
             print(word)
         end
